cc369baf06c833fe66402d57f125ed75309673d1
wcgrizolli <Walan.Grizolli@anl.gov>
2017-06-26 16:05:11 -0500

    added functions for stepping grating

--- wavepy/grating_interferometry.py
+++ wavepy/grating_interferometry.py
@@ -1053,3 +1053,146 @@ def plot_integration(integrated, pixelsize,
 
     return ax
 
+
+def _plot_chi2_stepping_grating(xvec, c_matrix_data, a_matrix, chi2):
+
+    axiscount = 0
+    xvecForGraph = np.linspace(np.min(xvec),
+                               np.max(xvec), 101)
+
+    plt.figure()
+    hist_y, hist_x, _ = plt.hist(chi2[np.where(chi2 < 100*np.std(chi2))],
+                                 100, log=False)
+    plt.title(r'$\chi^2$')
+
+    peak_chi2 = hist_x[np.argmax(hist_y)]
+
+    fwhm_chi2_1 = np.min(hist_x[np.where(hist_y > 0.5*np.max(hist_y))])
+    fwhm_chi2_2 = np.max(hist_x[np.where(hist_y > 0.5*np.max(hist_y))])
+
+    list_arg_chi2 = []
+
+    for i in range(4):
+
+        list_arg_chi2.append(np.argmin((hist_x - np.min(hist_x) -
+                             (fwhm_chi2_1 - np.min(hist_x))/4*i)**2))
+
+        list_arg_chi2.append(np.argmin((hist_x - fwhm_chi2_1 -
+                             (peak_chi2 - fwhm_chi2_1)/4*i)**2))
+        list_arg_chi2.append(np.argmin((hist_x - peak_chi2 -
+                             (fwhm_chi2_2-peak_chi2)/4*i)**2))
+        list_arg_chi2.append(np.argmin((hist_x - fwhm_chi2_2 -
+                             (np.max(hist_x)-fwhm_chi2_2)/4*i)**2) - 1)
+
+    list_arg_chi2.sort()
+
+    plt.plot(hist_x[list_arg_chi2], hist_y[list_arg_chi2], 'or',
+             label=r'values for fit plot')
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+    ii = np.mgrid[0:chi2.shape[0]-1:16j].astype(int)
+
+    f, axarr = plt.subplots(4, 4, figsize=(10, 8))
+
+    for i in chi2.argsort()[ii]:
+
+        ax = axarr.flatten()[axiscount]
+
+        ax.plot(xvec, c_matrix_data[:, i], '-ko')
+
+        ax.plot(xvecForGraph,
+                a_matrix[0, i] +
+                a_matrix[1, i]*np.sin(2*np.pi*xvecForGraph) +
+                a_matrix[2, i]*np.cos(2*np.pi*xvecForGraph), '--r')
+
+        ax.annotate(r'$\chi$ = {:.3g}'.format(chi2[i]),
+                    xy=(.80, .80), xycoords='axes fraction',
+                    xytext=(-20, 20), textcoords='offset pixels', fontsize=10,
+                    bbox=dict(boxstyle="round", fc="0.9"))
+
+        ax.grid()
+
+        if axiscount >= 12:
+            ax.set_xlabel('Grating Steps [gr period units]')
+
+        if axiscount % 4 == 0:
+            ax.set_ylabel('Counts')
+
+        axiscount += 1
+
+    plt.suptitle('Intensity in a single pixel with fit',
+                 fontsize=16, weight='bold')
+    plt.show(block=True)
+
+def _fit_stepping_grating(img_stack, gratingPeriod, stepSize, plotFits=True):
+
+    nsteps, nlines, ncolums = img_stack.shape
+
+    xg = np.linspace(0.0, (nsteps-1)*stepSize, nsteps)
+
+    c_matrix_data = img_stack.reshape((nsteps, nlines*ncolums))
+
+    bigB_matrix = np.zeros((nsteps, 3))
+    bigB_matrix[:, 0] = 1.0
+    bigB_matrix[:, 1] = np.sin(2*np.pi*xg/gratingPeriod)
+    bigB_matrix[:, 2] = np.cos(2*np.pi*xg/gratingPeriod)
+
+    bigG_matrix = np.dot(np.linalg.inv(np.dot(np.transpose(bigB_matrix),
+                                              bigB_matrix)),
+                         np.transpose(bigB_matrix))
+
+    a_matrix = np.dot(bigG_matrix, c_matrix_data)
+    c_matrix_model = np.dot(bigB_matrix, a_matrix)
+
+    chi2 = 1 / (nsteps - 3 - 1) * np.sum((c_matrix_data - c_matrix_model)**2 /
+                                         np.abs(c_matrix_data), axis=0)
+
+    if plotFits:
+        _plot_chi2_stepping_grating(xg/gratingPeriod,
+                                    c_matrix_data, a_matrix, chi2)
+
+    return (a_matrix.reshape((3, nlines, ncolums)),
+            chi2.reshape((nlines, ncolums)))
+
+
+def stepping_grating_1Danalysis(img_stack, ref_stack,
+                                period_oscilation, stepSize):
+    """
+    Function to process the data of stteping grating 1D imaging. It
+    wraps other functions in order to make all the process transparent
+
+    """
+
+    # fit sample stack
+    a_matrix_sample, chi2_sample = _fit_stepping_grating(img_stack[:, :, :],
+                                                         period_oscilation,
+                                                         stepSize,
+                                                         plotFits=True)
+
+    # fit ref stack
+    a_matrix_ref, chi2_ref = _fit_stepping_grating(ref_stack[:, :, :],
+                                                   period_oscilation,
+                                                   stepSize, plotFits=False)
+
+    # Obtain physical proprerties and plot
+    # Intensity
+    intensity = a_matrix_sample[0]/a_matrix_ref[0]
+
+    # Dark Field
+    dk_field_sample = np.sqrt(a_matrix_sample[2, :]**2 +
+                              a_matrix_sample[1, :]**2)/a_matrix_sample[0, :]
+
+    dk_field_ref = np.sqrt(a_matrix_ref[2, :]**2 +
+                           a_matrix_ref[1, :]**2)/a_matrix_ref[0, :]
+
+    dk_field = dk_field_sample/dk_field_ref
+
+    # DPC
+    dpc_1d = np.arctan2(a_matrix_sample[2, :], a_matrix_sample[1, :]) - \
+        np.arctan2(a_matrix_ref[2, :], a_matrix_ref[1, :])
+
+    dpc_1d = unwrap_phase(dpc_1d)
+
+    return intensity, dk_field, dpc_1d, chi2_ref
